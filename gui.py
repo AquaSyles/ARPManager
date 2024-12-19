f9aa44eec8bd4ba4ea71057e244b03000fddc2be
aqua <aqua@aqua.localdomain>
2024-12-19 12:39:24 +0100

    added some basic gui design

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QWidget, QVBoxLayout, QPushButton, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QLabel
 from PyQt6.QtCore import Qt
 
 import sys
@@ -12,22 +12,75 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Title')
         self.setGeometry(0, 0, self.windowSizeWidth, self.windowSizeHeight)
 
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
+        # Here I define the centralWidget to be a new widget
+        centralWidget = QWidget()
+        self.setCentralWidget(centralWidget)
 
-        # Create a vertical layout
-        self.layout = QVBoxLayout()        
+        # Layouts
+        outsideLayout = QVBoxLayout()
+        # Layouts to add to the outside layout/main layout
+        consoleContainerLayout = QVBoxLayout()        
 
-        # Add widgets to the layout
+        consoleColumnContainerLayout = QHBoxLayout()
+        consoleColumnKnownLayout = QHBoxLayout()
+        consoleColumnUnknownLayout = QHBoxLayout()
+
+        consoleRowLayout = QHBoxLayout()
+        consoleCommandLayout = QVBoxLayout()
+
+
+        buttonLayout = QHBoxLayout() 
+
+        # Define widgets to addd to layout here
+        self.columnKnownNameLabel = QLabel('Name', self)
+        self.columnKnownNameLabel.setStyleSheet('background-color: grey; color: black;')
+        self.columnKnownIpLabel = QLabel('IP', self)
+        self.columnKnownIpLabel.setStyleSheet('background-color: white; color: black;')
+        self.columnKnownMacLabel = QLabel('MAC', self)
+        self.columnKnownMacLabel.setStyleSheet('background-color: grey; color: black;')
+
+        self.columnUnknownIpLabel = QLabel('IP', self)
+        self.columnUnknownIpLabel.setStyleSheet('background-color: grey; color: black;')
+        self.columnUnknownMacLabel = QLabel('MAC', self)
+        self.columnUnknownMacLabel.setStyleSheet('background-color: white; color: black;')
+
         self.knownConsole = QTextEdit(self)
         self.unknownConsole = QTextEdit(self)
+        self.updateButton = QPushButton(text='Update')
 
-        self.layout.addWidget(self.knownConsole)        
-        self.layout.addWidget(self.unknownConsole)        
+        # Add widgets to layouts here
+        consoleColumnKnownLayout.addWidget(self.columnKnownNameLabel)
+        consoleColumnKnownLayout.addWidget(self.columnKnownIpLabel)
+        consoleColumnKnownLayout.addWidget(self.columnKnownMacLabel)
 
-        # Set the layout to the central widget
-        central_widget.setLayout(self.layout)
+        consoleColumnUnknownLayout.addWidget(self.columnUnknownIpLabel)
+        consoleColumnUnknownLayout.addWidget(self.columnUnknownMacLabel)
 
+        consoleRowLayout.addWidget(self.knownConsole)        
+        consoleRowLayout.addWidget(self.unknownConsole)   
+        buttonLayout.addWidget(self.updateButton)
+
+        # Set centralWidget to use outsideLayout which is the main layout
+        centralWidget.setLayout(outsideLayout)
+        # Add layouts to the main layout which is vertical
+        outsideLayout.addLayout(consoleContainerLayout)
+        outsideLayout.addLayout(buttonLayout)
+        # Add layouts to the consoleContainerLayout
+        consoleColumnContainerLayout.addLayout(consoleColumnKnownLayout)
+        consoleColumnContainerLayout.addLayout(consoleColumnUnknownLayout)
+        consoleContainerLayout.addLayout(consoleColumnContainerLayout)
+
+        consoleContainerLayout.addLayout(consoleRowLayout)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+
+        width = event.size().width()
+        height = event.size().height()
+
+        self.updateButton.setFixedSize(int(width/5), int(height/9))
+
+        print(f"Window size: {width}x{height}")
 
 app = QApplication(sys.argv)
 window = MainWindow()
